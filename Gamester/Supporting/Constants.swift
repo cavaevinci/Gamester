c210fcf555c84d184ab0f581788cf646f82b3896
cavaevinci <ivanevacicweb+private@gmail.com>
2024-02-14 22:14:53 +0100

    removed storyboard

---
+++ Gamester/Supporting/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Gamester
+//
+//  Created by Nino on 14.02.2024..
+//
+
+import Foundation
+
+struct Constants {
+    
+    //MARK: - API
+    static let scheme = "https"
+    static let baseURL = "xx"
+    static let port: Int? = nil
+    static let fullURL = "xx/"
+    
+    static let API_KEY = "13fe1f87745f4050a78bed37593efbdc"
+    
+}
---
+++ Gamester/Supporting/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Gamester
+//
+//  Created by Nino on 14.02.2024..
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        self.window = window
+        self.window?.makeKeyAndVisible()
+    }
+
+}
+
