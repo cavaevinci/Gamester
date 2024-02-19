e7ab413dc8cb0c4ddd0c62ae89f5c970bd396cc9
cavaevinci <ivanevacicweb+private@gmail.com>
2024-02-19 15:38:22 +0100

    added app icon,refactor api service

--- Gamester/Services/APIService.swift
+++ Gamester/Services/APIService.swift
@@ -15,41 +15,18 @@ class APIService: APIServiceProtocol {
     }
     
     func fetchData<T: Decodable>(from endpoint: APIEndpoint, responseType: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
-        
-        if case .gamesInGenre(let genreID) = endpoint {
             var urlComponents = URLComponents(string: endpoint.urlString)!
-            urlComponents.queryItems = [
-                URLQueryItem(name: "key", value: Constants.API_KEY),
-                URLQueryItem(name: "genres", value: "\(genreID)")
-            ]
             
-            guard let url = urlComponents.url else {
-                completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
-                return
+            if case .gamesInGenre(let genreID) = endpoint {
+                urlComponents.queryItems = [
+                    URLQueryItem(name: "key", value: Constants.API_KEY),
+                    URLQueryItem(name: "genres", value: "\(genreID)")
+                ]
+            } else {
+                urlComponents.queryItems = [
+                    URLQueryItem(name: "key", value: Constants.API_KEY)
+                ]
             }
-            URLSession.shared.dataTask(with: url) { (data, response, error) in
-                if let error = error {
-                    completion(.failure(error))
-                    return
-                }
-                
-                guard let data = data else {
-                    completion(.failure(NSError(domain: "No data received", code: 1, userInfo: nil)))
-                    return
-                }
-                
-                do {
-                    let decodedResponse = try JSONDecoder().decode(T.self, from: data)
-                    completion(.success(decodedResponse))
-                } catch {
-                    completion(.failure(error))
-                }
-            }.resume()
-        } else {
-            var urlComponents = URLComponents(string: endpoint.urlString)!
-            urlComponents.queryItems = [
-                URLQueryItem(name: "key", value: Constants.API_KEY),
-            ]
             
             guard let url = urlComponents.url else {
                 completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
@@ -75,5 +52,4 @@ class APIService: APIServiceProtocol {
                 }
             }.resume()
         }
-    }
 }
