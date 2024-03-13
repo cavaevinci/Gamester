7e0844e15c1816ac61833435db1bc4f3529e99b6
cavaevinci <ivanevacicweb+private@gmail.com>
2024-03-13 20:31:38 +0100

    add genre decoding unit tests

---
+++ GamesterTests/GenreTests.swift
@@ -0,0 +1,48 @@
+//
+//  GenreTests.swift
+//  GamesterTests
+//
+//  Created by Nino on 13.03.2024..
+//
+
+import XCTest
+@testable import Gamester
+
+final class GenreTests: XCTestCase {
+    
+    var sut: Genre!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        sut = nil
+    }
+
+    func testGenreDecodingSuccess() throws {
+        guard let path = Bundle(for: GenreTests.self).path(forResource: "MockGenreResponse", ofType: "json") else {
+            XCTFail("MockGenreResponse.json file not found")
+            return
+        }
+
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {
+            XCTFail("Failed to read data from file")
+            return
+        }
+
+        let decoder = JSONDecoder()
+        do {
+            sut = try decoder.decode(Genre.self, from: data)
+            XCTAssertEqual(sut.id, 0)
+            XCTAssertEqual(sut.name, "string")
+            XCTAssertEqual(sut.slug, "string")
+            XCTAssertEqual(sut.gamesCount, 0)
+        } catch {
+            XCTFail("Failed to decode JSON: \(error)")
+        }
+    }
+
+
+}
