b4fb09c16810e59db3b4119ce6a96c317665e4d9
cavaevinci <ivanevacicweb+private@gmail.com>
2024-04-09 02:40:08 +0200

    added app coordinator

---
+++ Gamester/Coordinators/AppCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  AppCoordinator.swift
+//  Gamester
+//
+//  Created by Nino on 09.04.2024..
+//
+
+import UIKit
+import SwiftyBeaver
+
+class AppCoordinator: Coordinator {
+    
+    let log = SwiftyBeaver.self
+    
+    var parentCoordinator: Coordinator?
+    
+    var children: [Coordinator] = []
+    
+    var navigationController: UINavigationController
+
+    init(navCon : UINavigationController) {
+        self.navigationController = navCon
+    }
+    
+    func start() {
+        log.debug("\(type(of: self)): Coordinator Start")
+    }
+}
